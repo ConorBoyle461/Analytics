37319a476b95a34d035f098208834c3790e2428f
Conor <conorboyle14@protonmail.com>
2021-03-25 23:14:08 +0000

    Adding in pyq csv parsing into q tbl object. Next step is send over handle to tp

--- scripts/python/pyqCsvLoader.py
+++ scripts/python/pyqCsvLoader.py
@@ -1,10 +1,7 @@
 from pyq import q
 from datetime import datetime
 import argparse
-
-
-q.connectTp =  q('{0N!x;hopen hsym x}')   #Takes string as input, eg q.connectTp("localhost:5000")
-
+import csv 
 
 def parseArgs():
     parser = argparse.ArgumentParser() 
@@ -15,17 +12,34 @@ def parseArgs():
     return args
 
 
+q.connect = q('{hopen hsym x}')
+q.tradeTbl = q('([]time:"n"$(); sym:`symbol$();price:`float$();size:`int$())')
+q.quoteTbl = q('([]time:"n"$(); sym:`symbol$();bid:`float$();ask:`float$(); bsize:`int$();asize:`int$())')
+q.aggTbl = q('([]time:"n"$(); sym:`symbol$() ; max_price:`float$() ; min_price: `float$(); volume:`int$())')
+q.header = q('{system raze "head -1 ",string x  }')
+q.createTbl = q('{flip (`$"," vs x)!()}')
+
 class loadCSV:
   def __init__(self,args):
      self.csv = args.csv
      self.tp = args.tp
      self.action = args.action
 
-  def connectTp(self):
-    print(self.tp)
+  def connect(self):
+    q.connectTp =  q('{hopen hsym x}')   #Takes string as input, eg q.connectTp("localhost:5000")
     self.handle = q.connectTp(self.tp)
+    
+  def readCsv(self):
+    with open(self.csv, "r") as csvfile:
+      read_lines = csv.reader(csvfile)
+      header = q.header(self.csv)
+      next(read_lines,None)
+      for row in read_lines:
+        q_tbl =(q.createTbl(header.first))
+        tbl = q_tbl.upsert(row)
+
 
 args = parseArgs()
 test = loadCSV(args)
-test.connectTp()
-print(test.handle)
+test.connect()
+test.readCsv()
